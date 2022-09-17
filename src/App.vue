6d4a91b8ba1243ca2075de1ac3d56277d3b7adeb
苏幕遮 <59789057+Yueser@users.noreply.github.com>
2022-09-17 14:49:50 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <img src="./assets/logo.png" alt="logo">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button @click="openNewView">el-button</el-button>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    openNewView(){
+      let url = window.location.hash;
+      window.location.href=url==='#/'?'#/about':'#/';
+    }
+  }
+}
+</script>
+
+<style>
+html{
+  overflow-y: auto;
+  overflow-x: hidden;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
+  margin-right: calc(100% - 100vw);
 }
 </style>
