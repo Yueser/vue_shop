5554a76e08b013ae328965e72b602cf34bf18829
苏幕遮 <59789057+Yueser@users.noreply.github.com>
2022-09-17 17:47:25 +0800

    login complete

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const router = new VueRouter({
     routes
 })
 router.beforeEach((to, from, next) => {
-    if (to.path == '/login') return next();
+    if (to.path === '/login') return next();
     const token = window.sessionStorage.getItem('token');
     if (!token) return next('/login');
     next();
